47147eee24830ccc5f2c84e06739d985bff1e308
zrcvae <2267085629@qq.com>
2024-09-27 23:00:11 +0800

    20240927V1.0

---
+++ src/main/java/com/yupi/yuoj/job/once/Test.java
@@ -0,0 +1,19 @@
+package com.yupi.yuoj.job.once;
+
+/**
+ * @author zrc
+ * @date 2024/07/02
+ */
+
+public class Test {
+
+    public static void main(String[] args) {
+        int i=12;
+
+        System.out.println(i= i +( i-(i * i)));
+    }
+}
+
+
+
+
